e15188d7199aa770cadc699e8e2a47e9866f22a7
mehmet kelnebioglu <mehmetkelnebioglu@gmail.com>
2023-02-22 18:16:17 -0500

    second comiit

---
+++ script.js
@@ -0,0 +1,9 @@
+ const words=["application","programming","interface"];
+
+window.addEventListener("keydown", (e) =>{
+    
+ if(e.keyCode >= 65 && e.keyCode <=90)
+    console.log("e",e.keyCode)
+
+});
+
