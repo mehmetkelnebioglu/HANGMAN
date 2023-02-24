ada3b3b69e695d96be11bd858a756a1525344785
mehmet kelnebioglu <mehmetkelnebioglu@gmail.com>
2023-02-24 09:47:56 -0500

    3. commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
  const words=["application","programming","interface"];
+ const selectedWord = words[Math.floor(Math.random()*words.length)];
+ console.log("seleceteed word",selectedWord);
+ const wrongLetters=document.getElementsByClassName("wrong-letters")
+
 
 window.addEventListener("keydown", (e) =>{
     
- if(e.keyCode >= 65 && e.keyCode <=90)
-    console.log("e",e.keyCode)
+ if(e.keyCode >= 65 && e.keyCode <=90){
 
+    const letter = e.key.toLocaleLowerCase()
+    console.log("letter",letter);
+    wrongLetters[0].innerHTML= `${wrongLetters[0].innerHTML} , ${letter}`;
+ }
+   
 });
 
