fc7ecdf53bbea571dd73feb55b51c8ed0d3264b7
mehmet kelnebioglu <mehmetkelnebioglu@gmail.com>
2023-02-24 18:34:30 -0500

    4. commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-/*  const words=["application","programming","interface"];
+ const words=["application","programming","interface"];
  const selectedWord = words[Math.floor(Math.random()*words.length)];
-const  corretLetters=document.getElementById('word')
- 
- const wrongLetters=document.getElementsByClassName("wrong-letters")
+const  corretLetters=document.getElementById('word');
+ const wrongLetters=document.getElementById("wrong-letters");
+
+ const corretLetterss= [];
+ const wrongLetterss=[];
+
 
 
 window.addEventListener("keydown", (e) =>{
@@ -10,21 +13,25 @@ window.addEventListener("keydown", (e) =>{
  if(e.keyCode >= 65 && e.keyCode <=90){
 
     const letter = e.key.toLocaleLowerCase()
-    console.log("letter",letter);
 
     if(selectedWord.includes(letter)){
-       corretLetters.innerHTML += letter+ ","
+        corretLetterss.push(letter)
+        corretLetters.innerHTML += letter+ ","
     }else{
-        wrongLetters[0].innerHTML += letter + ","
+        wrongLetterss.push(letter);
+        updateWrongLetters();
     }
     
    }
 });
 
- */
+function updateWrongLetters(){
+    console.log("updateWrongLetters")
+    wrongLetters.innerHTML = `${wrongLetters.map((letter)=>letter)}`;
 
-let a=5
+}
 
-   console.log(a+=1) 
+
+
 
 
